942cd6251641f85be31d579a49da61a29e31bd51
Anton Filippovich <antonfilippovich.job@gmail.com>
2024-09-22 13:22:20 +0300

    fix: update build configs

--- packages/icon/vite.config.ts
+++ packages/icon/vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 export default defineConfig({
-    plugins: [react(), dts({ include: ['src'] })],
+    plugins: [react(), libInjectCss(), dts({ include: ['src'] })],
     build: {
+        copyPublicDir: false,
+        rollupOptions: {
+            external: ['react', 'react/jsx-runtime', 'react-icons'],
+        },
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
-            name: 'Icon',
             formats: ['es'],
-        },
-        rollupOptions: {
-            external: ['react', 'react-dom', 'react/jsx-runtime', 'react-icons'],
         },
     },
 });
