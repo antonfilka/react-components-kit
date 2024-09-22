8537fadbd65ad1eb129983249de64700b7090d4b
Anton Filippovich <antonfilippovich.job@gmail.com>
2024-09-22 13:54:04 +0300

    feat: refactor all

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Button as AntButton, ButtonProps as AntButtonProps } from 'antd';
+import { Icon } from '@antonfilka/icon';
 
 export interface ButtonProps extends AntButtonProps {}
 
 export const Button: React.FC<ButtonProps> = ({ ...props }) => {
-    return <AntButton {...props}></AntButton>;
+    return (
+        <AntButton {...props}>
+            <Icon />
+        </AntButton>
+    );
 };
